c4752523aa316cd0d7a620aac2f8c9de7ddda40a
Abhishek Anvekar <anvekara1@gmail.com>
2024-04-23 04:29:46 +0530

    circuit breaker pattern and Retry pattern implemented

--- employee-service/src/main/java/com/employee/employeeservice/service/serviceImpl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/employee/employeeservice/service/serviceImpl/EmployeeServiceImpl.java
@@ -8,8 +8,12 @@ import com.employee.employeeservice.exception.ResourceNotFoundException;
 import com.employee.employeeservice.repositories.EmployeeRepository;
 import com.employee.employeeservice.service.APIClient;
 import com.employee.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,6 +30,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 //    private WebClient webClient;
     private APIClient apiClient;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     @Override
     public EmployeeDto addEmployee(EmployeeDto employeeDto) {
         Employee employee = modelMapper.map(employeeDto, Employee.class);
@@ -75,8 +81,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return "Employee deleted successfully";
     }
 
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeAndDepartmentByEmployeeId(long id) {
+
+        LOGGER.info("Inside getEmployeeAndDepartmentByEmployeeId() method....");
         Employee employee = employeeRepo.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Employee", "id", id)
         );
@@ -91,6 +101,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //code for FeignClient
         DepartmentDto departmentDto = apiClient.getDepartmentByCode(employee.getDepartmentCode());
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+        return apiResponseDto;
+    }
+
+    public APIResponseDto getDefaultDepartment(long id, Exception exception) {
+        LOGGER.info("Inside getDefaultDepartment() method....");
+
+        Employee employee = employeeRepo.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "id", id)
+        );
+        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+
+
+        //default DepartmentDto
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setName("R&D");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDescription("Research And Development");
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployeeDto(employeeDto);
